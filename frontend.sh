5f25850152478a8214795dd1bbc7e60b5a1bcf5b
naveenkumar <naveen6655@gmail.com>
2024-05-03 18:01:10 +0530

    adding print commands to to frondent script

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,19 @@
+echo installing nginx
 dnf install nginx -y
+
+echo copy expense config file
 cp expense.conf /etc/nginx/default.d/expense.conf
+
+echo clean old content
 rm -rf /usr/share/nginx/html/*
+
+echo download frontend content
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 cd /usr/share/nginx/html
+
+echo extract the content
 unzip /tmp/frontend.zip
+
+echo restart the service
 systemctl restart nginx
 systemctl enable nginx
