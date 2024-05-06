4c39ecb1302bec5c6adc78beaea8c3bc8d116602
naveenkumar <naveen6655@gmail.com>
2024-05-06 17:49:19 +0530

    using functions in frontend

--- frontend.sh
+++ frontend.sh
@@ -1,27 +1,30 @@
 rm -f /tmp/expense.log
 
-echo -e "\e[36mInstalling nginx\e[0m"
+HEADING(){
+  echo -e "\e[35m$*\e[0m"
+}
+HEADING Installing nginx
 dnf install nginx -y &>> /tmp/expense.log
 echo exit status - $?
 
-echo -e "\e[36mCopy expense config file\e[0m"
+HEADING Copy expense config file
 cp expense.conf /etc/nginx/default.d/expense.conf  &>> /tmp/expense.log
 echo exit status - $?
 
-echo -e "\e[36mClean old content\e[0m"
+HEADING Clean old content
 rm -rf /usr/share/nginx/html/*  &>> /tmp/expense.log
 echo exit status - $?
 
-echo -e "\e[36mDownload frontend content\e[0m"
+HEADING Download frontend content
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>> /tmp/expense.log
 cd /usr/share/nginx/html
 echo exit status - $?
 
-echo -e "\e[36mExtract the content\e[0m"
+HEADING Extract the content
 unzip /tmp/frontend.zip &>> /tmp/expense.log
 echo exit status - $?
 
-echo -e "\e[36mRestart the service\e[0m"
+HEADING Restart the service
 systemctl restart nginx &>> /tmp/expense.log
 systemctl enable nginx &>> /tmp/expense.log
 echo exit status - $?
