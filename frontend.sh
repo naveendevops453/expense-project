ec95da36328738a17c8d0840be01842fd709401c
naveenkumar <naveen6655@gmail.com>
2024-05-06 16:56:19 +0530

    Redirecting the log output to expense.log

--- frontend.sh
+++ frontend.sh
@@ -1,25 +1,25 @@
 echo -e "\e[36mInstalling nginx\e[0m"
-dnf install nginx -y
+dnf install nginx -y &> /tmp/expense.log
 echo exit status - $?
 
 echo -e "\e[36mCopy expense config file\e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf
+cp expense.conf /etc/nginx/default.d/expense.conf  &> /tmp/expense.log
 echo exit status - $?
 
 echo -e "\e[36mClean old content\e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/*  &> /tmp/expense.log
 echo exit status - $?
 
 echo -e "\e[36mDownload frontend content\e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &> /tmp/expense.log
 cd /usr/share/nginx/html
 echo exit status - $?
 
 echo -e "\e[36mExtract the content\e[0m"
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &> /tmp/expense.log
 echo exit status - $?
 
 echo -e "\e[36mRestart the service\e[0m"
-systemctl restart nginx
-systemctl enable nginx
+systemctl restart nginx &> /tmp/expense.log
+systemctl enable nginx &> /tmp/expense.log
 echo exit status - $?
