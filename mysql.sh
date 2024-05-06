811df123a397a8425bbb6e897cffa4c8e5e88ca9
naveenkumar <naveen6655@gmail.com>
2024-05-06 16:38:31 +0530

    Adding special variables with context

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,12 @@
+echo -e "\e[36mInstalling Mysql server\e[0m"
 dnf install mysql-server -y
+echo exit status - $?
+
+echo -e "\e[36Enabling and starting mysqld service\e[0m"
 systemctl enable mysqld
 systemctl start mysqld
-mysql_secure_installation --set-root-pass ExpenseApp@1
+echo exit status - $?
+
+echo -e "\e[36mUpdating with password\e[0m"
+mysql_secure_installation --set-root-pass $1
+echo exit status - $?
