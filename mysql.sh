91cf88c11d6b5388778d7fb5326fefdc10c0de17
naveenkumar <naveen6655@gmail.com>
2024-05-07 07:07:25 +0530

    adding output to a file

--- mysql.sh
+++ mysql.sh
@@ -1,11 +1,12 @@
 source common.sh
+rm -f /tmp/expense.log
 if [ -z "$1" ]; then
   echo "Input MySQL password is missing"
   exit 1
 fi
 
 HEADING Installing Mysql server
-dnf install mysql-server -y
+dnf install mysql-server -y &>> /tmp/expense.log
 if [ $? -eq 0 ]; then
   echo SUCCESS
 else
@@ -14,8 +15,8 @@ else
 fi
 
 HEADING Enabling and starting mysqld service
-systemctl enable mysqld
-systemctl start mysqld
+systemctl enable mysqld &>> /tmp/expense.log
+systemctl start mysqld &>> /tmp/expense.log
 if [ $? -eq 0 ]; then
   echo SUCCESS
 else
@@ -24,7 +25,7 @@ else
 fi
 
 HEADING Updating with password
-mysql_secure_installation --set-root-pass $1
+mysql_secure_installation --set-root-pass $1 &>> /tmp/expense.log
 if [ $? -eq 0 ]; then
   echo SUCCESS
 else
